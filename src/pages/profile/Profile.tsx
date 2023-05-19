48c62c5bebce31392348aadc6e835ca2d8227315
Tim Kent <timdekent@gmail.com>
2023-05-18 21:01:28 -0700

    Add change password form

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -5,6 +5,10 @@ import AuthContext from "../../context/AuthContext";
 import checkLocalStorage from "../../utils/check-local-storage";
 import useUser from "../../hooks/useUser";
 import QueryError from "../../components/errors/queryError";
+import { CustomStylesEnum } from "../../models/styles";
+import Form from "../../components/forms/Form";
+import Input from "../../components/forms/Input";
+import Button from "../../components/forms/Button";
 
 const Profile = () => {
   // auth check
@@ -45,11 +49,27 @@ const Profile = () => {
   return (
     <div>
       <h2>My Profile</h2>
-      <div>
-        <p>Email: {userData.userEmail}</p>
-        <p>Nickname: {userData.userNickname}</p>
+      <div className="border-t my-4 py-4">
+        <p className="mb-2.5">Email: {userData.userEmail}</p>
+        <p className="mb-2.5">Nickname: {userData.userNickname}</p>
+        <div
+          className={`${CustomStylesEnum.authButton} ${CustomStylesEnum.btnPrimary} text-center`}>
+          <Link to="/profile/edit">Edit</Link>
+        </div>
       </div>
-      <Link to="/profile/edit">Edit</Link>
+      <div className="border-t my-4 py-4">
+        <h4>Change Password</h4>
+        <div className="my-4">
+          <Form id="change-password-form">
+            {/* // TODO: change type to password */}
+            <Input type="text" id="" name="" label="New Password" handleChange={() => {}} />
+            <Input type="text" id="" name="" label="Confirm New Password" handleChange={() => {}} />
+            <Input type="text" id="" name="" label="Current Password" handleChange={() => {}} />
+            <Button type="submit" text="Submit" handleClick={() => {}} />
+            <Button type="button" text="Cancel" handleClick={() => {}} />
+          </Form>
+        </div>
+      </div>
       <Outlet />
     </div>
   );
