a1aa5b6a89dfbc73cee3598d83dbe42c98e7f6f1
Tim Kent <timdekent@gmail.com>
2023-06-02 09:40:17 -0700

    adjust inner modal div max h

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -23,7 +23,7 @@ const ModalContent = ({ modalContent }: ModalContentProps) => {
     <div
       className={`z-30 fixed w-full h-screen overflow-auto left-0 bg-white rounded-lg top-8
 lg:w-[calc(1024px*0.6)] lg:top-[92px] lg:h-[70%] lg:left-[calc(50%-(1024px*0.6/2))] lg:shadow-xl`}>
-      <div className="max-lg:p-4 max-lg:overflow-auto max-lg:relative max-lg:w-full max-lg:max-w-full max-lg:max-h-screen">
+      <div className="max-lg:p-4 max-lg:overflow-y-auto max-lg:relative max-lg:w-full max-lg:max-w-full max-lg:max-h-[calc(100vh-2rem)]">
         {active && modal.active && <ShowApiError errorData={data!} />}
         {modalContent}
       </div>
