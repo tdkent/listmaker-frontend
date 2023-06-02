29c870d4a1a4c63745c958b3c7250d956b110340
Tim Kent <timdekent@gmail.com>
2023-06-02 09:06:37 -0700

    update modal to allow mobile scroll

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -14,11 +14,19 @@ const ModalContent = ({ modalContent }: ModalContentProps) => {
   const { active, data } = useContext(ErrorContext);
   const modal = useContext(ModalContext);
   const modalDisplay = (
+    //   <div
+    //     className={`z-30 absolute lg:shadow-xl w-full h-screen overflow-auto left-0 bg-white p-2 rounded-lg top-8
+    // lg:w-[calc(1024px*0.6)] lg:top-[92px] lg:h-[70%] lg:left-[calc(50%-(1024px*0.6/2))]`}>
+    //     {active && modal.active && <ShowApiError errorData={data!} />}
+    //     {modalContent}
+    //   </div>
     <div
-      className={`z-30 absolute lg:shadow-xl w-full h-screen overflow-auto left-0 bg-white p-2 rounded-lg top-8
-  lg:w-[calc(1024px*0.6)] lg:top-[92px] lg:h-[70%] lg:left-[calc(50%-(1024px*0.6/2))]`}>
-      {active && modal.active && <ShowApiError errorData={data!} />}
-      {modalContent}
+      className={`z-30 fixed w-full h-screen overflow-auto left-0 bg-white rounded-lg top-8
+lg:w-[calc(1024px*0.6)] lg:top-[92px] lg:h-[70%] lg:left-[calc(50%-(1024px*0.6/2))] lg:shadow-xl`}>
+      <div className="max-lg:p-4 max-lg:overflow-auto max-lg:relative max-lg:w-full max-lg:max-w-full max-lg:max-h-screen">
+        {active && modal.active && <ShowApiError errorData={data!} />}
+        {modalContent}
+      </div>
     </div>
   );
   return ReactDOM.createPortal(modalDisplay, document.getElementById("modal")!);
